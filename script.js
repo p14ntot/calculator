e50410ccd4a421762f2ddbb5f7cdad4b3b58fdcb
p14ntot <p14ntot@ionio.gr>
2023-03-21 20:00:48 +0200

    add btns and result-box

---
+++ script.js
@@ -0,0 +1,25 @@
+
+
+
+function add(a,b){
+    
+    console.log(a+b);
+}
+
+function subtract(a,b){
+    
+    console.log(a-b);
+}
+
+function multiply(a,b){
+    
+    console.log(a*b);
+}
+
+function devide(a,b){
+    
+    console.log(a/b);
+}
+
+
+
